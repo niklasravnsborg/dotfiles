8ef4548cc6393bb4a1709a4aa8c78e7851d46b8e
Marjan Grabowski <git@knaggit.de>
2022-12-14 15:34:59 +0100

    Update install.sh and add PATH parameters

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+## Run these three commands in your terminal to add Homebrew to your PATH:
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/mgr/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/mgr/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install dependencies with brew
 brew bundle --file=macos/Brewfile
