8e56505dbe15c6b99f44e2264a83ace878105e7e
Niklas Ravnsborg <niklasravnsborg@gmail.com>
2026-09-16 00:22:12 +0200

    fix Claude theming via SSH and inside Tmux

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,11 @@
 set fish_greeting
 set sponge_purge_only_on_exit true
 
+# SSH forwards TERM, but not COLORTERM. Ghostty uses xterm-256color here.
+if set -q SSH_CONNECTION; and test "$TERM" = xterm-256color; and not set -q COLORTERM
+    set -gx COLORTERM truecolor
+end
+
 # Set PATH and environment
 # If TERM_PROGRAM is not tmux, since tmux will already have sourced the environment
 if test -z "$TMUX" -a "$TERM_PROGRAM" != tmux
